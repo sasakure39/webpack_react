40d8414b30af412762b9e5c41efe2fa32803d286
yuh takamura <yuhtakamura@gmail.com>
2016-09-13 19:15:32 +0900

    Comment.js作成

---
+++ frontend/src/comment/Comment.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'marked'
+
+export default React.createClass({
+  rawMarkup: function() {
+    let rawMarkup = marked(this.props.children.toString(), {sanitize: true})
+    return { __html: rawMarkup }
+  },
+
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    )
+  }
+})
